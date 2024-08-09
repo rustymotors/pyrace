09184d106915aa0b67b281157dc345273685770e
Molly Draven <drazi@duck.com>
2024-08-09 05:52:42 -0400

    chore: Update licensing information in code files

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-from distutils import core
+# RustyMotors is a project to build an online server for a legacy racing game
+# Copyright (C) 2024 Molly Draven
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from pyrace.shared.config import getConfig
 import sentry_sdk
 
